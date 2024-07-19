3f5293d47844ef7bafec302ae6af79bce0e2dba9
Rull30 <ramadhan69628@gmail.com>
2024-07-19 14:57:38 +0700

    fix title yang masih error

--- resources/views/admin/rehab-medik/index.blade.php
+++ resources/views/admin/rehab-medik/index.blade.php
@@ -11,14 +11,14 @@
         <div class="content-header-left col-md-9 col-12 mb-2">
             <div class="row breadcrumbs-top">
                 <div class="col-12">
-                    <h4 class="content-header-title float-start mb-0">Data rehab-medik</h4>
+                    <h4 class="content-header-title float-start mb-0">Data Rehab Medik</h4>
                     <div class="breadcrumb-wrapper">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
                                 <a href="#">Beranda</a>
                             </li>
                             <li class="breadcrumb-item active">
-                                <a href="#">Daftar Farmasi</a>
+                                <a href="#">Daftar Rehab Medik</a>
                             </li>
                         </ol>
                     </div>
