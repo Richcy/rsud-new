650a6185ce8748015115290f5bf647ea97d28914
Rull30 <ramadhan69628@gmail.com>
2024-07-22 23:10:43 +0700

    menambahkan fitur untuk gallery dan membuat menambahakan sub service

--- app/Http/Controllers/User/LayananUnggulanController.php
+++ app/Http/Controllers/User/LayananUnggulanController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Models\Gallery;
 use App\Models\Service;
+use App\Models\SubService;
 use App\Models\RunningText;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,7 +14,9 @@ class LayananUnggulanController extends Controller
     public function index()
     {
         $unggulan = Service::where('type', 'unggulan')->first();
+        $subService = SubService::where('service_id', $unggulan->id)->get();
+        $image = Gallery::where('type', 'unggulan')->orderBy('sort', 'asc')->get();
         $running_text = RunningText::first();
-        return view('user.layanan_unggulan.index', compact('unggulan', 'running_text'));
+        return view('user.layanan_unggulan.index', compact('unggulan', 'running_text', 'subService', 'image'));
     }
 }
--- resources/views/user/layanan_unggulan/index.blade.php
+++ resources/views/user/layanan_unggulan/index.blade.php
@@ -8,13 +8,26 @@
 @section('seo_url', route('user.layanan_unggulan.index'))
 @extends('user.layouts.main')
 @push('custom_css')
+   <!-- Splide CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.11/dist/css/splide.min.css">
+   <!-- GLightbox CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
     <style>
-       @media(min-widht: 768px)
-       {
-        #description{
-            margin-left: 20px;
+        @media(min-widht: 768px)
+        {
+            #description{
+                margin-left: 20px;
+            }
         }
-       }
+        .accordion-item {
+            border: none !important;
+        }
+        .accordion-button {
+            color: #121212 !important;
+            background: transparent !important;
+            box-shadow: none !important;
+            border: none !important;
+        }
     </style>
 @endpush
 
@@ -33,13 +46,104 @@
                     <h3>PROFIL RSUD CIMACAN</h3>
                 </div>
 
+                @if ($image != null)
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-8">
+                        <div class="container pt-5" id="description">
+                            {!! $unggulan->description !!}
+                        </div>
+                        <div class="accordion" id="accordionExample">
+                            @forelse ($subService as $item)
+                              <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingOne">
+                                      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                          <i style="color: #A82024; font-size: 20px;" class="fa-solid fa-square-plus me-3"></i> {{ $item->title }}
+                                      </button>
+                                  </h2>
+                                  <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                      <div class="accordion-body container">
+                                          {!! $item->description !!}
+                                      </div>
+                                  </div>
+                              </div>
+                            @empty
+
+                            @endforelse
+                          </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="text-center">
+                            <h3>Galeri</h3>
+                        </div>
+                        <div id="imageCarousel" class="splide">
+                            <div class="splide__track">
+                                <ul class="splide__list">
+                                    @foreach ($image as $item)
+                                        <li class="splide__slide">
+                                            <a href="{{ asset('storage/' . $item->img) }}" data-glightbox="title: {{ $item->title }}">
+                                                <img src="{{ asset('storage/' . $item->img) }}" style="transform: scale(0.7)" class="d-block w-100" alt="...">
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @else
+            <div class="col-12">
                 <div class="container pt-5" id="description">
                     {!! $unggulan->description !!}
                 </div>
+                <div class="accordion" id="accordionExample">
+                    @forelse ($subService as $item)
+                      <div class="accordion-item">
+                          <h2 class="accordion-header" id="headingOne">
+                              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  <i style="color: #A82024; font-size: 20px;" class="fa-solid fa-square-plus me-3"></i> {{ $item->title }}
+                              </button>
+                          </h2>
+                          <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                              <div class="accordion-body container">
+                                  {!! $item->description !!}
+                              </div>
+                          </div>
+                      </div>
+                    @empty
+
+                    @endforelse
+                  </div>
+            </div>
+            @endif
+
+
+
             </div>
         </div>
     </div>
 @endsection
 
 @push('custom_js')
+     <!-- Splide JS -->
+     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.11/dist/js/splide.min.js"></script>
+     <!-- GLightbox JS -->
+     <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            // Initialize Splide.js
+            new Splide('#imageCarousel', {
+                type: 'slide',
+                perPage: 1,
+                perMove: 1,
+                pagination: false,
+                arrows: true,
+            }).mount();
+
+            // Initialize GLightbox
+            const lightbox = GLightbox({
+                selector: 'a[data-glightbox]',
+            });
+        });
+    </script>
+
 @endpush
