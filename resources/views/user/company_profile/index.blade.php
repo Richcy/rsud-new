23a3e65360f3208677277cee2ed2d75415b70007
Rull30 <ramadhan69628@gmail.com>
2024-07-22 15:11:46 +0700

    membuat halaman tentang rumah sakit untuk user

---
+++ resources/views/user/company_profile/index.blade.php
@@ -0,0 +1,38 @@
+
+@section('seo_keyword', 'Beranda, Halaman Utama, rumah sakit umum daerah cimacan, rsud cimacan, rsd cimacan')
+@section('seo_title', 'RSUD Cimacan | Beranda')
+@section('seo_desc',
+    'Rumah Sakit Umum Daerah Cimacan pada awalnya adalah Puskesmas Pacet (Cimacan) yang sudah berdiri
+    sejak tahun 1953, kemudian pada tahun 1981 statusnya meningkat menjadi Puskesmas DTP dan berubah status menjadi Rumah
+    Sakit dengan ditetapkannya Surat Keputusan Bupati Cianjur atas nama Pemerintah Daerah Kabupaten Cianjur Nomor 19 Tahu…')
+@section('seo_url', )
+@extends('user.layouts.main')
+@push('custom_css')
+
+@endpush
+
+@section('content')
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <span><small><a href="">Beranda</a> / <a href="">Tentang</a> / Profil Rumah Sakit</small></span>
+                <div class="text-center">
+                    <img style="height: 250px; width: 100%; object-fit:cover;" src="{{ asset('storage/'. $profile->banner) }}" alt="">
+                </div>
+            </div>
+
+            <div class="col-12 my-4">
+                <div class="text-left">
+                    <h3>PROFIL RSUD CIMACAN</h3>
+                </div>
+
+                <div class="container mx-3 pt-5">
+                    {!! $profile->description !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('custom_js')
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\HomeController;
 use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\User\CompanyProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,10 @@ use App\Http\Controllers\Admin\Auth\LoginController;
 
 require __DIR__ . '/administrator.php';
 
-Route::get('/', [HomeController::class, 'index'])->name('user.home');
+Route::name('user.')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [CompanyProfileController::class, 'index'])->name('profile.index');
+});
 
 Route::post('/logout', [LoginController::class, 'logout'])
                 ->middleware('auth')
